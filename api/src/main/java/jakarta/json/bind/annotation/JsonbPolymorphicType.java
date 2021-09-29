148166549813822fbee52d6e2a537aa16a056713
David Kral <david.k.kral@oracle.com>
2021-09-29 14:03:28 +0200

    Jsonb polymorphism support
    Signed-off-by: David Kral <david.k.kral@oracle.com>

---
+++ api/src/main/java/jakarta/json/bind/annotation/JsonbPolymorphicType.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package jakarta.json.bind.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Configuration annotation of the polymorphic type handling.
+ * <br>
+ * This annotation is required to be present on the most top of all the classes,
+ * polymorphism should be applied to.
+ * <pre>{@code
+ * // Example
+ * @JsonbPolymorphicType(keyName = "@key")
+ * interface Animal {}
+ *
+ * class Dog implements Animal {}
+ * class Cat implements Animal {}
+ * }</pre>
+ * This annotation is closely tight with {@link JsonbSubtype}. It is recommended to use
+ * {@link JsonbSubtype} annotations to specify all the possible classes and their aliases.
+ * <br>
+ */
+@JsonbAnnotation
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface JsonbPolymorphicType {
+
+    /**
+     * Key name used for keeping polymorphic information when {@link Format#PROPERTY} is chosen.
+     *
+     * @return key name
+     */
+    String keyName() default "";
+
+    /**
+     * Specification of how the polymorphic information will be stored in the resulting JSON.
+     *
+     * @return polymorphic information storage type
+     */
+    Format format() default Format.PROPERTY;
+
+    /**
+     * Whether exact class names should be processed if no alias is specified for processed class.
+     * <br>
+     * Default value is false.
+     *
+     * @return classes without alias should be processed
+     */
+    boolean classNames() default false;
+
+    /**
+     * Whitelisted package names. This option is ignored if {@link JsonbPolymorphicType#classNames()}
+     * option is set to false.
+     * <br>
+     * Only classes contained in the selected packages will be serialized/deserialized.
+     * Classes with specified alias are not validated.
+     * <br>
+     * It is strongly recommended having whitelist set up if classes without alias should be processed.
+     * <br>
+     * When no whitelist is specified, all classes without alias are allowed.
+     *
+     * @return package whitelist
+     */
+    String[] whitelist() default "";
+
+    /**
+     * Format under which serialized polymorphic type should be handled.
+     * <br>
+     * Default polymorphic format is {@link Format#PROPERTY}.
+     */
+    enum Format {
+
+        /**
+         * Serialized object will be wrapped with another object.
+         * This wrapping object will contain only one property with the name of the
+         * polymorphic information (alias/class name) and a value assigned to this property is an actual
+         * serialized object.
+         * <br>
+         * This format ignores specified {@link JsonbPolymorphicType#keyName()}.
+         * <pre>{@code
+         * // Example
+         * @JsonbPolymorphicType(format = Format.WRAPPING_OBJECT)
+         * @JsonbSubtype(alias = "dog", type = Dog.class)
+         * interface Animal {}
+         *
+         * class Dog implements Animal {
+         *     public String isDog = true;
+         * }
+         *
+         * //Resulting json after dog instance serialization
+         * {"dog":{"isDog":true}}
+         * }</pre>
+         */
+        WRAPPING_OBJECT,
+
+        /**
+         * Serialized object will be in form of json array with two values.
+         * <br>
+         * The first in the array is polymorphic information (alias/class name) and the second
+         * array value is an actual serialized object.
+         * <br>
+         * This format ignores specified {@link JsonbPolymorphicType#keyName()}.
+         * <pre>{@code
+         * // Example
+         * @JsonbPolymorphicType(format = Format.WRAPPING_ARRAY)
+         * @JsonbSubtype(alias = "dog", type = Dog.class)
+         * interface Animal {}
+         *
+         * class Dog implements Animal {
+         *     public String isDog = true;
+         * }
+         *
+         * //Resulting json after dog instance serialization
+         * ["dog",{"isDog":true}]
+         * }</pre>
+         */
+        WRAPPING_ARRAY,
+
+        /**
+         * Serialized object will have one more property added to the resulting json.
+         * This property will contain polymorphic information (alias/class name).
+         * <br>
+         * It is required to have nonempty {@link JsonbPolymorphicType#keyName()} specified when
+         * using this format.
+         * <pre>{@code
+         * // Example
+         * @JsonbPolymorphicType(keyname = "@type", format = Format.PROPERTY)
+         * @JsonbSubtype(alias = "dog", type = Dog.class)
+         * interface Animal {}
+         *
+         * class Dog implements Animal {
+         *     public String isDog = true;
+         * }
+         *
+         * //Resulting json after dog instance serialization
+         * {"@type":"dog","isDog":true}
+         * }</pre>
+         * This is the default format of the polymorphic handling and does not need to be
+         * explicitly specified.
+         */
+        PROPERTY
+
+    }
+
+}
---
+++ tck/src/main/java/jakarta/json/bind/tck/defaultmapping/polymorphictypes/AnnotationPolymorphismTest.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package jakarta.json.bind.tck.defaultmapping.polymorphictypes;
+
+import java.lang.invoke.MethodHandles;
+import java.time.LocalDate;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbConfig;
+import jakarta.json.bind.JsonbException;
+import jakarta.json.bind.annotation.JsonbCreator;
+import jakarta.json.bind.annotation.JsonbDateFormat;
+import jakarta.json.bind.annotation.JsonbPolymorphicType;
+import jakarta.json.bind.annotation.JsonbProperty;
+import jakarta.json.bind.annotation.JsonbSubtype;
+import jakarta.json.bind.config.PropertyOrderStrategy;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Tests for verification of proper polymorphism handling based on annotation with property format.
+ */
+@RunWith(Arquillian.class)
+public class AnnotationPolymorphismTest {
+
+    private final Jsonb jsonb = JsonbBuilder.create();
+
+    @Deployment
+    public static WebArchive createTestArchive() {
+        return ShrinkWrap.create(WebArchive.class)
+                .addPackages(true, MethodHandles.lookup().lookupClass().getPackage().getName());
+    }
+
+    @Test
+    public void testBasicSerialization() {
+        Dog dog = new Dog();
+        String jsonString = jsonb.toJson(dog);
+        if (!jsonString.matches("\\{\\s*\"@type\"\\s*:\\s*\"dog\"\\s*,\\s*\"isDog\"\\s*:\\s*true\\s*\\}")) {
+            fail("Failed to serialize Dog class correctly in the polymorphic format: "
+                         + JsonbPolymorphicType.Format.PROPERTY.name());
+        }
+        Cat cat = new Cat();
+        jsonString = jsonb.toJson(cat);
+        if (!jsonString.matches("\\{\\s*\"@type\"\\s*:\\s*\"cat\"\\s*,\\s*\"isCat\"\\s*:\\s*true\\s*\\}")) {
+            fail("Failed to serialize Cat class correctly in the polymorphic format: "
+                         + JsonbPolymorphicType.Format.PROPERTY.name());
+        }
+    }
+
+    @Test
+    public void testBasicDeserialization() {
+        Animal dog = jsonb.fromJson("{\"@type\":\"dog\",\"isDog\":false}", Animal.class);
+        if (!(dog instanceof Dog)) {
+            fail("Incorrectly deserialized to the polymorphic type. Expected was Dog instance. "
+                         + "Got instance of class " + dog.getClass());
+            return;
+        }
+        if (((Dog) dog).isDog) {
+            fail("Incorrectly deserialized field of the Dog instance. Field \"isDog\" should have been false.");
+            return;
+        }
+        Animal cat = jsonb.fromJson("{\"@type\":\"cat\",\"isCat\":false}", Animal.class);
+        if (!(cat instanceof Cat)) {
+            fail("Incorrectly deserialized to the polymorphic type. Expected was Cat instance. "
+                         + "Got instance of class " + cat.getClass());
+            return;
+        }
+        if (((Cat) cat).isCat) {
+            fail("Incorrectly deserialized field of the Cat instance. Field \"isCat\" should have been false.");
+        }
+    }
+
+    @Test
+    public void testExactTypeDeserialization() {
+        Dog dog = jsonb.fromJson("{\"isDog\":false}", Dog.class);
+        if (dog.isDog) {
+            fail("Incorrectly deserialized field of the Dog instance. Field \"isDog\" should have been false.");
+            return;
+        }
+        dog = jsonb.fromJson("{\"@type\":\"dog\", \"isDog\":false}", Dog.class);
+        if (dog.isDog) {
+            fail("Incorrectly deserialized field of the Dog instance. Field \"isDog\" should have been false.");
+        }
+    }
+
+    @Test
+    public void testUnknownAliasDeserialization() {
+        try {
+            jsonb.fromJson("{\"@type\":\"rat\",\"isRat\":false}", Animal.class);
+            fail("Deserialization should fail. Alias \"rat\" is not valid alias.");
+        } catch (JsonbException ignored) {
+            //Expected
+        }
+    }
+
+    @Test
+    public void testUnknownAliasSerialization() {
+        try {
+            jsonb.toJson(new Rat());
+            fail("Serialization should fail. There is no known alias for a class Rat.");
+        } catch (JsonbException ignored) {
+            //Expected
+        }
+    }
+
+    @Test
+    public void testCreatorDeserialization() {
+        SomeDateType creator = jsonb
+                .fromJson("{\"@dateType\":\"constructor\",\"localDate\":\"26-02-2021\"}", SomeDateType.class);
+
+        if (!(creator instanceof DateConstructor)) {
+            fail("Incorrectly deserialized to the polymorphic type. Expected was DateConstructor instance. "
+                         + "Got instance of class " + creator.getClass());
+        }
+    }
+
+    @Test
+    public void testArraySerialization() {
+        String expected = "\\["
+                + "\\s*\\{\\s*\"@type\"\\s*:\\s*\"dog\"\\s*,\\s*\"isDog\"\\s*:\\s*true\\s*\\}\\s*,"
+                + "\\s*\\{\\s*\"@type\"\\s*:\\s*\"cat\"\\s*,\\s*\"isCat\"\\s*:\\s*true\\s*\\}\\s*,"
+                + "\\s*\\{\\s*\"@type\"\\s*:\\s*\"dog\"\\s*,\\s*\"isDog\"\\s*:\\s*true\\s*\\}\\s*"
+                + "\\]";
+        Animal[] animals = new Animal[] {new Dog(), new Cat(), new Dog()};
+        String jsonString = jsonb.toJson(animals);
+        if (!jsonString.matches(expected)) {
+            fail("Array values were not properly serialized with polymorphic information.");
+        }
+    }
+
+    @Test
+    public void testArrayDeserialization() {
+        String array = "[{\"@type\":\"dog\",\"isDog\":true},{\"@type\":\"cat\",\"isCat\":true},"
+                + "{\"@type\":\"dog\",\"isDog\":true}]";
+        Animal[] deserialized = jsonb.fromJson(array, Animal[].class);
+        if (deserialized.length != 3) {
+            fail("Array should have exactly 3 values.");
+        } else if (!(deserialized[0] instanceof Dog)) {
+            fail("Array value at index 0 was incorrectly deserialized to the polymorphic type. Expected was Dog instance. "
+                         + "Got instance of class " + deserialized[0].getClass());
+        } else if (!(deserialized[1] instanceof Cat)) {
+            fail("Array value at index 1 was incorrectly deserialized to the polymorphic type. Expected was Cat instance. "
+                         + "Got instance of class " + deserialized[1].getClass());
+        } else if (!(deserialized[2] instanceof Dog)) {
+            fail("Array value at index 2 was incorrectly deserialized to the polymorphic type. Expected was Dog instance. "
+                         + "Got instance of class " + deserialized[2].getClass());
+        }
+    }
+
+    @Test
+    public void testOrderStrategyIgnored() {
+        Jsonb jsonb = JsonbBuilder.create(new JsonbConfig().withPropertyOrderStrategy(PropertyOrderStrategy.REVERSE));
+        Elephant elephant = new Elephant();
+        String jsonString = jsonb.toJson(elephant);
+        if (!jsonString.matches("\\{\\s*\"@type\"\\s*:\\s*\"elephant\"\\s*,"
+                                        + "\\s*\"testProperty\"\\s*:\\s*\"value\"\\s*,"
+                                        + "\\s*\"isElephant\"\\s*:\\s*true\\s*"
+                                        + "\\}")) {
+            fail("PropertyOrderStrategy " + PropertyOrderStrategy.REVERSE + " was not ignored for added property with "
+                         + "polymorphic information");
+        }
+        jsonb = JsonbBuilder.create(new JsonbConfig().withPropertyOrderStrategy(PropertyOrderStrategy.LEXICOGRAPHICAL));
+        jsonString = jsonb.toJson(elephant);
+        if (!jsonString.matches("\\{\\s*\"@type\"\\s*:\\s*\"elephant\"\\s*,"
+                                        + "\\s*\"isElephant\"\\s*:\\s*true\\s*,"
+                                        + "\\s*\"testProperty\"\\s*:\\s*\"value\"\\s*"
+                                        + "\\}")) {
+            fail("PropertyOrderStrategy " + PropertyOrderStrategy.LEXICOGRAPHICAL + " was not ignored for added property with "
+                         + "polymorphic information");
+        }
+    }
+
+    @JsonbPolymorphicType(keyName = "@type")
+    @JsonbSubtype(alias = "dog", type = Dog.class)
+    @JsonbSubtype(alias = "cat", type = Cat.class)
+    @JsonbSubtype(alias = "elephant", type = Elephant.class)
+    public interface Animal {
+
+    }
+
+    public static class Dog implements Animal {
+
+        public boolean isDog = true;
+
+    }
+
+    public static class Cat implements Animal {
+
+        public boolean isCat = true;
+
+    }
+
+    public static class Rat implements Animal {
+
+        public boolean isRat = true;
+
+    }
+
+
+    public static class Elephant implements Animal {
+
+        public boolean isElephant = true;
+        public String testProperty = "value";
+
+    }
+
+    @JsonbPolymorphicType(keyName = "@dateType")
+    @JsonbSubtype(alias = "constructor", type = DateConstructor.class)
+    public interface SomeDateType {
+
+    }
+
+    public static final class DateConstructor implements SomeDateType {
+
+        public LocalDate localDate;
+
+        @JsonbCreator
+        public DateConstructor(@JsonbProperty("localDate") @JsonbDateFormat(value = "dd-MM-yyyy", locale = "nl-NL") LocalDate localDate) {
+            this.localDate = localDate;
+        }
+
+    }
+
+}
